51abcce47c95af7f872ecee0d44475ea75dfe141
Dheeraj <dheeraj.java92@gmail.com>
2018-08-05 19:21:40 +0530

    added Constant class and changes in App.java

--- src/main/java/com/tech/git_demo/App.java
+++ src/main/java/com/tech/git_demo/App.java
@@ -9,7 +9,7 @@ public class App
 	
 	public void addData(String data){
     	System.out.println("data :" + data);
-    	if(data.equals("java-techie")){
+    	if(data.equals(Constant.Data_Type)){
     		System.out.println("jjjjjjjjjjjjj");
     	}
     }
---
+++ src/main/java/com/tech/git_demo/Constant.java
@@ -0,0 +1,6 @@
+package com.tech.git_demo;
+
+public class Constant {
+	public static String Data_Type ="java-techie";
+
+}
